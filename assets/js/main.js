098bcb5c08a28ac7dbe668ad29cc6d1e914c0dad
Marre24 <Maximilian.ellnestam@gmail.com>
2026-04-08 21:50:30 +0200

    Add preferences

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
+const targetElement = document.getElementById("alcoholPreferences");
+
 function sendResponse() {
   const name = document.getElementById("name").value.trim();
   const canCome = document.getElementById("canCome").checked;
   const wantAlcohol = document.getElementById("wantAlcohol").checked;
   const allergies = document.getElementById("allergies").value.trim();
+  const preferences = targetElement.value.trim();
 
   var body = `Hejsan Maxi,
 
@@ -13,7 +16,10 @@ ${
 }
 ${
   wantAlcohol
-    ? "Jag skulle gärna vilja att du köper alkohol till mig."
+    ? "Jag skulle gärna vilja att du köper alkohol till mig." + preferences ==
+      ""
+      ? ""
+      : `Mina önskemål gällande alkohol är: ${preferences}`
     : "Du behöver inte köpa alkohol till mig."
 }
 ${allergies == "" ? "" : `Här är mina allergier/saker du bör känna till: ${allergies}`}
@@ -30,3 +36,14 @@ Mvh, ${name}`;
 
   window.location.href = link;
 }
+
+const checkbox = document.getElementById("wantAlcohol");
+targetElement.style.display = "none";
+
+checkbox.addEventListener("click", function handleClick() {
+  if (checkbox.checked) {
+    targetElement.style.display = "block";
+  } else {
+    targetElement.style.display = "none";
+  }
+});
