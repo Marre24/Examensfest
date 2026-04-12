04a7cbfb6674daf3e067a1687c1f573186649fb9
Marre24 <Maximilian.ellnestam@gmail.com>
2026-04-12 21:24:33 +0200

    Update the content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,30 @@
-const targetElement = document.getElementById("alcoholPreferences");
-
 function sendResponse() {
   const name = document.getElementById("name").value.trim();
   const canCome = document.getElementById("canCome").checked;
-  const wantAlcohol = document.getElementById("wantAlcohol").checked;
-  const preferences = targetElement.value.trim();
+  const wantBubbel = document.getElementById("wantBubbel").checked;
+  const wantAlkFriBubbel = document.getElementById("wantAlkFriBubbel").checked;
+  const wantBål = document.getElementById("wantBål").checked;
 
   var body = `Hejsan Maxi,
 
 ${
   canCome
-    ? "Jag skulle gärna vilja komma på din asfeta examensfest."
-    : "Tyvärr kan jag inte komma på din examensfest."
+    ? "Jag skulle gärna vilja komma på din asfeta examensfest. "
+    : "Tyvärr kan jag inte komma på din examensfest. "
 }
 ${
-  wantAlcohol
-    ? "Jag skulle gärna vilja att du köper alkohol till mig." + preferences ==
-      ""
-      ? ""
-      : `Mina önskemål gällande alkohol är: ${preferences}`
-    : "Du behöver inte köpa alkohol till mig."
-}
+  wantBubbel
+    ? "Jag vill att du köper skitfin dompa(bubbel) till mig. "
+    : "Men det där bubbliga är inte för mig. "
+}${
+    wantAlkFriBubbel
+      ? "Jag skulle gärna vilja att du köper bubbel sans alkohol tack. "
+      : "Alkoholfritt bubbel? Nej tack jag tar med alkohol istället. "
+  }${
+    wantBål
+      ? "OOoooooo bål låter skitnajs, fixa lite till mig också. "
+      : "Det här med bål är inte för mig. "
+  }
 
 Mvh, ${name}`;
 
@@ -53,17 +57,6 @@ function showModal(message) {
 function closeModal() {
   document.getElementById("xp-modal").style.display = "none";
 }
-
-const checkbox = document.getElementById("wantAlcohol");
-targetElement.style.display = "none";
-
-checkbox.addEventListener("click", function handleClick() {
-  if (checkbox.checked) {
-    targetElement.style.display = "block";
-  } else {
-    targetElement.style.display = "none";
-  }
-});
 
 document.getElementById("name").addEventListener("input", function () {
   const name = this.value.trim();
